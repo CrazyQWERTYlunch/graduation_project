253b7e73430e404c644bec2027a9a4e52bae0658
UzNik <uznik.99@mail.ru>
2024-02-09 22:16:57 +0400

    base calendar has been added

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from routes.models import Route
 
-# Create your models here.
+class Events(models.Model):
+    # name = models.ForeignKey(Route)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,null=True,blank=True)
+    start = models.DateTimeField(null=True,blank=True)
+    end = models.DateTimeField(null=True,blank=True)
+ 
+    class Meta:  
+        db_table = "tblevents"
---
+++ schedule/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from routes import views
+
+app_name = 'shedule'
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('all_events/', views.all_events, name='all_events'), 
+    path('add_event/', views.add_event, name='add_event'), 
+    path('update/', views.update, name='update'),
+    path('remove/', views.remove, name='remove'),
+]
+
+
+
+    
